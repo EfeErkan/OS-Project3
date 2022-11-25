3cbf2420dff35485a7f0dbd654e6d39745f640ef
Efe Erkan <72245656+EfeErkan@users.noreply.github.com>
2022-11-25 12:03:05 +0300

    preparation of page_table based on input

--- page_table.h
+++ page_table.h
@@ -1,5 +1,68 @@
 #ifndef PAGE_TABLE
 #define PAGE_TABLE
 
+#include <stdlib.h>
+#include "address_processing.h"
+
+#define PAGE_TABLE1_LEN 1024
+#define PAGE_TABLE2_LEN 1024
+
+#define USED '1'
+#define UNUSED '0'
+
+//Definitions
+struct page_table1_entry
+{
+    char available;
+    struct page_table2_entry *second_level;
+};
+
+struct page_table2_entry
+{
+    int frame_number;
+};
+
+struct page_table1_entry *init_page_table();
+
+void free_page_table(struct page_table1_entry *page_table);
+
+void set_page_table(char hex1[HEX_LENGTH], char hex2[HEX_LENGTH]);
+
+// Implementation
+
+struct page_table1_entry *init_page_table()
+{
+    struct page_table1_entry *page_table = malloc(sizeof(struct page_table1_entry) * PAGE_TABLE1_LEN);
+
+    for (int i = 0; i < PAGE_TABLE1_LEN; i++)
+    {
+        page_table[i].available = UNUSED;
+        page_table[i].second_level = NULL;
+    }
+}
+
+void free_page_table(struct page_table1_entry *page_table)
+{
+    for (int i = 0; i < PAGE_TABLE1_LEN; i++)
+    {
+        if (page_table[i].available == USED)
+        {
+            free(page_table[i].second_level);
+        }
+    }
+    free(page_table);
+}
+
+void set_page_table_interval(struct page_table1_entry *page_table, char hex1[HEX_LENGTH], char hex2[HEX_LENGTH])
+{
+    int start = get_page_part1(hex1);
+    int end = get_page_part1(hex2);
+
+    for (int i = start; i < end; i++)
+    {
+        page_table[i].available = USED;
+        page_table[i].second_level = malloc(sizeof(struct page_table2_entry) * PAGE_PART2_LENGTH);
+    }
+}
 
 #endif
--- pagism.c
+++ pagism.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-#include "address_processing.h"
+#include "page_table.h"
 
 #define MIN_CMD_LENGTH 7
 #define MAX_CMD_LENGTH 9
 #define STR_LEN 64
 #define LRU 1
 #define FIFO 2
+#define BASE_ADDRESS "0x00000000"
 
 int main(int argc, char const *argv[])
 {
     char in1[STR_LEN];
     char in2[STR_LEN];
+    char out[STR_LEN];
     int num_of_frames;
     int algorithm;
     char vmsize[HEX_LENGTH];
@@ -26,11 +29,13 @@ int main(int argc, char const *argv[])
     else
     {
         strcpy(in1, argv[1]); strcpy(in2, argv[2]);
-        num_of_frames = atoi(argv[3]); algorithm = atoi(argv[5]);
+        num_of_frames = atoi(argv[3]);
+        strcpy(out, argv[4]); 
+        algorithm = atoi(argv[6]);
 
         if ( argc == MAX_CMD_LENGTH )
         {
-            strcpy(vmsize, argv[9]);
+            strcpy(vmsize, argv[8]);
             vmmode = 1;
         }
         else
@@ -39,6 +44,27 @@ int main(int argc, char const *argv[])
             exit(-1);
         }
 
+        struct page_table1_entry *page_table = init_page_table();
+
+        FILE *fp_intervals = fopen(in1, "r");
+        FILE *fp_addresses = fopen(in2, "r");
+        FILE *fp_out = fopen(out, "w");
+
+        if (vmmode == 0)
+        {
+            char address1[HEX_LENGTH];
+            char address2[HEX_LENGTH];
+
+            while ( fscanf(fp_intervals, "%s", address1) != EOF )
+            {
+                fscanf(fp_intervals, "%s", address2);
+                set_page_table_interval(page_table, address1, address2);
+            }
+        }
+        else if (vmmode == 1)
+        {
+            set_page_table_interval(page_table, BASE_ADDRESS, vmsize);
+        }
         
     }
 
